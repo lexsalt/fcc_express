5f693df71b56d588f4804c7739a2aa1177f7ad42
emilianosal <emilianosal@hotmail.com>
2023-08-21 11:16:56 +0200

    Challenge 8/12 completed

--- myApp.js
+++ myApp.js
@@ -42,7 +42,14 @@ app.get("/json", function(req, res) {
   }
 });
 
-
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString()
+  next()
+}, function(req,res) {
+  res.json({
+    time: req.time
+  })
+} )
 
 
 // arrow function
