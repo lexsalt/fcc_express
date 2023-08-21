fea4ef18ac0c5c05b55cab02b881c794f96b58b7
emilianosal <emilianosal@hotmail.com>
2023-08-21 10:00:18 +0200

    first commit

--- myApp.js
+++ myApp.js
@@ -1,7 +1,67 @@
 let express = require('express');
 let app = express();
+require("dotenv").config()
+console.log(process.env.MESSAGE_STYLE)
+
+console.log('Hello World')
+
+// app.get("/", function(req, res) {
+//     res.send("Hello World");
+//   });
+
+//app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// app.get("/", function(req, res) {
+//   res.sendFile(__dirname + "/views/index.html");
+// });
+
+app.get("/json", function(req, res) {
+  res.json({ message: 'Hello json'});
+});
+
+// arrow function
+// app.get("/json", (req, res) => {
+//   res.json({
+//     message: "Hello json"
+//   });
+// });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
 
 
 
